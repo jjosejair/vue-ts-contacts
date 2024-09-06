29b483d4fad4ee8d1ddb74582e6f72f2e589e503
jjosejair <jairsdc@hotmail.com>
2024-09-06 16:23:40 -0300

    mudança

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://66bfa33c42533c403146b83b.mockapi.io/contacts/v1',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { loadEnvFile } from 'node:process'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+  const API_URL = `${env.VITE_API_BASE_URL ?? ""}`
+  return {
+    plugins: [
+      vue()
+    ]
+    ,
+    server: {
+      proxy: {
+        '^/api': {
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      }
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
