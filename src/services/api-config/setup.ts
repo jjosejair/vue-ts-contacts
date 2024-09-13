dd86feb5e294f03e664acc937da5d38c2a10e0bc
jjosejair <jairsdc@hotmail.com>
2024-09-13 15:35:08 -0300

    I added the generic GET, POST, PUT, PATH and DELETE and axios methods and the configuration of the interceptors.

---
+++ src/services/api-config/setup.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, InternalAxiosRequestConfig } from "axios";
+
+
+function apiConfig(baseUrl: string): AxiosRequestConfig {
+  return {
+    baseURL: baseUrl,
+  };
+}
+
+function initAxios(config: AxiosRequestConfig, token?: any): AxiosInstance {
+  const defineInstance = axios.create(config);
+
+  defineInstance.interceptors.request.use(
+    (request: InternalAxiosRequestConfig) => {
+      return request;
+    },
+    (error) => Promise.reject(error)
+  );
+
+  defineInstance.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+      alert(error);
+      return Promise.reject(error);
+    }
+  );
+
+  return defineInstance;
+}
+
+function api(baseURL= "/api", token?: any) {
+  return initAxios(apiConfig(baseURL), token);
+}
+
+export default api;
